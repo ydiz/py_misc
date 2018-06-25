ff23490563d254134343dea4ad1eabcb69cbf161
yyzz <zyd2117@gmail.com>
2018-06-24 21:30:41 -0400

    Revert "tt"
    This reverts commit 7309eaa2e66516c8afb31ee409bd71d5e1d13b66.

--- GF_hmc.py
+++ GF_hmc.py
@@ -7,21 +7,16 @@ def log_name(M, trajL, innerMC_N_force, innerMC_N_H):
 
 grid = (4,4,4,4)
 # U = coldConfiguration((4,)+grid+(3,3))
-# Umu = readField("U_softly_fixed_4")
-# U = np.moveaxis(Umu, 4, 0)
+Umu = readField("U_softly_fixed_4")
+U = np.moveaxis(Umu, 4, 0)
 trajN = 1
 trajL = 0.01
 MDsteps = 1
-# innerMC_N_force = 1000
-# innerMC_N_H = 1000
-hb_offset = 1000
+innerMC_N_force = 100
+innerMC_N_H = 100
+hb_offset = 100
 hb_multi_hit = 10
 M = 1.0
-
-for innerMC_N_force in (500, 1000, 3000, 10000):
-    for innerMC_N_H in (500, 1000, 3000, 10000):
-        Umu = readField("U_softly_fixed_4")
-        U = np.moveaxis(Umu, 4, 0)
-        log_file = log_name(M, trajL, innerMC_N_force, innerMC_N_H)
-        action = GFAction(5.6, M, innerMC_N_force, innerMC_N_H, hb_offset, hb_multi_hit, log_file)
-        HMC(U, trajN, trajL, MDsteps, action)
+log_file = log_name(M, trajL, innerMC_N_force, innerMC_N_H)
+action = GFAction(5.6, M, innerMC_N_force, innerMC_N_H, hb_offset, hb_multi_hit, log_file)
+HMC(U, trajN, trajL, MDsteps, action)
