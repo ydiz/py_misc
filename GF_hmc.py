cbbb03da4fd0f2c1068707ffab7f02cf625a82f4
yyzz <zyd2117@gmail.com>
2018-06-26 13:04:47 -0400

    2222

--- GF_hmc.py
+++ GF_hmc.py
@@ -5,12 +5,15 @@ grid = (4,4,4,4)
 U = coldConfiguration((4,)+grid+(3,3))
 # Umu = readField("U_softly_fixed_4")
 # U = np.moveaxis(Umu, 4, 0)
-trajN = 3
-trajL = 0.01
+trajN = 1
+trajL = 0.03
 MDsteps = 1
-innerMC_N = 10000
-hb_offset = 100
+# innerMC_N = 1000
+innerMC_N_force = 1000
+innerMC_N_H = 1000
+hb_offset = 1000
 hb_multi_hit = 10
 M = 1.0
-action = GFAction(5.6, M, innerMC_N, hb_offset, hb_multi_hit)
+
+action = GFAction(5.6, M, innerMC_N_force, innerMC_N_H, hb_offset, hb_multi_hit)
 HMC(U, trajN, trajL, MDsteps, action)
